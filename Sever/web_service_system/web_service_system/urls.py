9692fffa5d99f4a10db158634fd1a5a1eebff37d
zc <ChengCapre@outlook.com>
2024-05-07 16:03:43 +0800

    add static

--- Sever/web_service_system/web_service_system/urls.py
+++ Sever/web_service_system/web_service_system/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("user/", include("user.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
