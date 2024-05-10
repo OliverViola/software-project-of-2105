7a21ef6bccd3f31bebe242a66a913fb8530812b1
unknown <1242790805@qq.com>
2024-05-10 22:01:39 +0800

    get data

---
+++ Web/web_service_system/src/components/Card.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    picUrl: String,
+    artworkTitle: String,
+    artworkMaterial: String,
+    id: String
+  },
+  methods : {
+    goToDetailPage() {
+      this.$router.push({ name: 'DetailsPage', params: {id: this.id}})
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="shadow-right-bottom flex flex-col p-4" :style="{height: '360px'}" @click="goToDetailPage">
+    <img :src="picUrl" :style="{height: '200px'}">
+    <div class="mt-2 flex flex-col gap-3">
+      <p class="title-text">{{artworkTitle}}</p>
+      <p class="text">{{artworkMaterial}}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.shadow-right-bottom {
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.3); /* 只在右下方显示阴影 */
+}
+
+.title-text {
+  font-size: 20px;
+  font-weight: 500;
+  text-decoration: underline;
+}
+
+.text {
+  overflow: hidden;
+}
+</style>
--- Web/web_service_system/src/components/Viewer.vue
+++ Web/web_service_system/src/components/Viewer.vue
@@ -5,14 +5,15 @@
     mounted() {
         const viewer = OpenSeadragon({
             id: "openseadragon1",//标签id
+            zoomInButton:   "zoom-in",
+            zoomOutButton:  "zoom-out",
+            fullPageButton: "expand",
             showNavigator: true, // 是否显示导航缩略图
+            dragCursor: 'none',
             prefixUrl: "https://openseadragon.github.io/openseadragon/images/", //插件小图标
             tileSources : {
                 type: "image",
-                // crossOriginPolicy: 'Anonymous',
-                // ajaxWithCredentials: false,
-                // url: "https://www.sxhm.com/Uploads/Picture/2021/11/11/s618c7eba48d28.png"
-                url: "https://gips3.baidu.com/it/u=113178756,797997158&fm=3039&app=3039&f=JPEG?w=1024&h=1024"
+                url: "https://www.chnmus.net/webfile/collection_img/_6666_/41000002_6666_A_1.JPG"
             }
         });
     }
@@ -20,5 +21,20 @@
 </script>
 
 <template>
-    <div id="openseadragon1" :style="{backgroundColor:'rgb(240, 240, 240)', width: '100%', height: 'calc(-12.5rem + 100dvh)', position: 'relative'}"></div>
+    <div>
+        <div class="relative">
+            <div id="openseadragon1" :style="{backgroundColor:'rgb(240, 240, 240)', width: '100%', height: 'calc(-10rem + 100dvh)', position: 'relative'}"></div>
+        </div>
+        <div class="controller-wrapper px-4 py-6 md:p-6 flex justify-between flex-col md:flex-row gap-4">
+            <div class="flex flex-row items-center gap-2 order-2 md:gap-4 md:order-1">
+                <div class="flex justify-between sm:justify-start w-full sm:w-auto gap-2 md:gap-4 sm:w-full md:w-full">
+                    <button id="zoom-in" class="button">放大</button>
+                    <button id="zoom-out" class="button">缩小</button>
+                </div>
+            </div>
+            <div class="flex items-center gap-2 md:gap-4 order-1 md:order-2">
+                <button id="expand" class="button">展开</button>
+            </div>
+        </div>
+    </div>
 </template>
