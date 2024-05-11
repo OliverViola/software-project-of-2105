02f53c8b11d5648ec54958b77b0f06fb285b63bc
zhang <ChengCapre@outlook.com>
2024-05-11 18:57:56 +0800

    getDetail

--- Sever/web_service_system/artifact/views/getInfo.py
+++ Sever/web_service_system/artifact/views/getInfo.py
@@ -1,9 +1,9 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from artifact.serializers import ArtifactSerializer
 
 from artifact.models import Artifact
-import random
 
 
 @api_view(["POST"])
@@ -15,3 +15,13 @@ def getRandomArtifact(request):
     except Exception as e:
         return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(["POST"])
+def getDetailArtifact(request):
+    try:
+        art_id = request.data.get("id")
+        artifact = Artifact.objects.get(id=art_id)
+        serializer = ArtifactSerializer(artifact)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
