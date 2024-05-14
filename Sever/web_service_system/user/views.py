76523dd9ace2ec426ae474fe4c070e11f608dafa
Arickkaa <arickkaa@outlook.com>
2024-05-14 17:03:55 +0800

    deal with username exists & email exists

--- Sever/web_service_system/user/views.py
+++ Sever/web_service_system/user/views.py
@@ -13,8 +13,22 @@ from .serializers import UserSerializer
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
+        username = request.data.get("username")
+        email = request.data.get("email")
+
+        if CustomUser.objects.filter(username=username).exists():
+            return Response(
+                {"message": "Username already exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if CustomUser.objects.filter(email=email).exists():
+            return Response(
+                {"message": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
