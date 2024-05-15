222e7f0df919b46996124e0228e107d83487b51b
zhang <ChengCapre@outlook.com>
2024-05-15 15:25:59 +0800

    edit search api

--- Sever/web_service_system/artifact/views/searchArtifacts.py
+++ Sever/web_service_system/artifact/views/searchArtifacts.py
@@ -10,13 +10,14 @@ from artifact.models import Artifact
 def searchArtifacts(request):
     try:
         prompt = request.data.get('prompt')
+        number = request.data.get('number')
         obj_artifacts = Artifact.objects.filter(Q(name__icontains=prompt) | Q(description__icontains=prompt)
                                                 | Q(type__icontains=prompt) | Q(time__icontains=prompt) |
                                                 Q(creator__icontains=prompt) | Q(size__icontains=prompt)
                                                 | Q(museum__icontains=prompt) | Q(material__icontains=prompt)
                                                 | Q(level__icontains=prompt) | Q(placeOfOrigin__icontains=prompt)
                                                 ).values()
-        artifact_list = list(obj_artifacts)
+        artifact_list = list(obj_artifacts)[:number]
         return Response(data=artifact_list, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
