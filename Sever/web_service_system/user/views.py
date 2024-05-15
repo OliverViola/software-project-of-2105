4d1231ac2176f0018b9270878b00b116fee6ffd2
zc <ChengCapre@outlook.com>
2024-05-15 15:44:32 +0800

    debug

--- Sever/web_service_system/user/views.py
+++ Sever/web_service_system/user/views.py
@@ -29,19 +29,6 @@ def register(request):
     if serializer.is_valid():
         username = request.data.get("username")
         email = request.data.get("email")
-
-        if CustomUser.objects.filter(username=username).exists():
-            return Response(
-                {"message": "Username already exists"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        if CustomUser.objects.filter(email=email).exists():
-            return Response(
-                {"message": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
