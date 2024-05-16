a104cb2b66f76cb5d3c28dba0177c022526850f4
unknown <1242790805@qq.com>
2024-05-16 21:26:35 +0800

    add timeline

--- Web/web_service_system/src/views/TimeLine.vue
+++ Web/web_service_system/src/views/TimeLine.vue
@@ -1,19 +1,17 @@
+// 时间轴加载需要魔法
+
 <template>
 <div>
   <Header></Header>
-  <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1zkxMfsAv1w0HMXHPF8wraYLSnZngi3oKM9u8ZeINn1s&font=Default&lang=zh-cn&initial_zoom=2&height=650&optimal_tick_width=200' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+  <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1zkxMfsAv1w0HMXHPF8wraYLSnZngi3oKM9u8ZeINn1s&font=Default&lang=zh-cn&initial_zoom=2&height=650&optimal_tick_width=300' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
 </div>
 </template>
 
-
 <script>
-// import {getCookie} from "@/utils/cookie";
-// import request from "@/utils/request";
 
 import Header from "@/components/Header.vue";
 
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
   name: "TimeLine",
   components: {
     Header
