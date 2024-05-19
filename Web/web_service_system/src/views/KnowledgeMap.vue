87005bdfd0a68ff5c6a0a7f9857dcd4024f80492
zhang <ChengCapre@outlook.com>
2024-05-19 15:54:43 +0800

    imbed neo4j

--- Web/web_service_system/src/views/KnowledgeMap.vue
+++ Web/web_service_system/src/views/KnowledgeMap.vue
@@ -1,84 +1,96 @@
 <template>
-  <el-card class="box-card">
-
-  </el-card>
+  <div style="height: 1000px; width: 100%;" ref="Screen">
+    <div class="left" id="viz" ref="viz1"></div>
+  </div>
 </template>
 
 
-<script setup>
-import Header from "../components/Header.vue";
-import NeoVis from 'neovis.js';
-import { onMounted } from "vue";
+<script>
+export default {
+  name: 'KnowledgeMap',
+  components: {},
+  props: {},
+  data() {
+    return {
+      viz: {} //定义一个viz对象
+    }
+  },
+  mounted() {
+    this.draw();
+  },
 
-onMounted(() => {
-  draw();
-})
+  methods: {
+    draw() {
+      var config = {
+        container_id: "viz",
+        server_url: "bolt://101.200.145.59:7687",
+        server_user: "neo4j",
+        server_password: "12345678",
+        labels: {
+          "info": {
+            "caption": "name",
+            "font": {
+              "size": 15,
+              "color": "#100000"
+            },
 
-var viz;
 
-function draw() {
-  var config = {
-    container_id: "viz",
-    server_url: "bolt://101.200.145.59:7474",
-    server_user: "neo4j",
-    server_password: "12345678",
-    labels: {
-      "info": {
-        "caption": "name",
-        "font": {
-          "size": 15,
-          "color": "#100000"
+          },
+          "relic": {
+            "caption": "name",
+            "font": {
+              "size": 25,
+              "color": "#000000"
+            },
+            "community": "community"
+          },
+
         },
-
-
-      },
-      "relic": {
-        "caption": "name",
-        "font": {
-          "size": 25,
-          "color": "#000000"
+        relationships: {
+          "年代": {
+            "thickness": "count",
+            "caption": true
+          },
+          "现藏博物馆": {
+            "thickness": "count",
+            "caption": true
+          },
+          "类型": {
+            "thickness": "count",
+            "caption": true
+          },
+          "藏品原产地": {
+            "thickness": "count",
+            "caption": true
+          },
+          "藏品材质": {
+            "thickness": "count",
+            "caption": true
+          },
+          "藏品等级": {
+            "thickness": "count",
+            "caption": true
+          },
         },
-        "community": "community"
-      },
+        arrows: true,
+        hierarchical: false,
+        initial_cypher: "MATCH p=()-[]->() RETURN p LIMIT 300"
 
-    },
-    relationships: {
-      "年代": {
-        "thickness": "count",
-        "caption": true
-      },
-      "现藏博物馆": {
-        "thickness": "count",
-        "caption": true
-      },
-      "类型": {
-        "thickness": "count",
-        "caption": true
-      },
-      "藏品原产地": {
-        "thickness": "count",
-        "caption": true
-      },
-      "藏品材质": {
-        "thickness": "count",
-        "caption": true
-      },
-      "藏品等级": {
-        "thickness": "count",
-        "caption": true
-      },
-    },
-    arrows: true,
-    hierarchical: false,
-    initial_cypher: "MATCH p=()-[]->() RETURN p LIMIT 300"
+      };
 
-  };
+      this.viz = new NeoVis.default(config);
+      this.viz.render();
+      console.log(viz);
 
-  viz = new NeoVis(config);
-  viz.render();
-  console.log(viz);
-
+    }
+  }
 }
-
-
 </script>
+
+<style scoped>
+#viz {
+  width: 100%;
+  height: 100%;
+  font: 22pt arial;
+}
+</style>
